b207e5c8d67f31ec3db5f3a73209f13fa23a1a1b
fourthten <agung0410@gmail.com>
2019-11-15 13:33:12 +0700

    kasus dasar

--- kemampuan-dasar/kemampuan-dasar-1/kasus/cari-java.bat
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/cari-java.bat
@@ -1,7 +1,9 @@
 @echo OFF
 echo masukkan direktori file yang akan dicek
 set /p "dr=>"
+@REM tampilkan list direktori
 DIR %dr% /w /s /a
+@REM cari file java
 for /R %dr% %%f in (*.java) do (
     echo Ada file Java pada direktori %%f
 )
--- kemampuan-dasar/kemampuan-dasar-1/kasus/file-ren.bat
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/file-ren.bat
@@ -2,11 +2,14 @@
 setlocal enabledelayedexpansion
 echo Masukkan direktori file yang akan dicek:
 set /p "dr=>"
+@REM tampilkan list direktori
 dir %dr% /w /s /a
+@REM cari file java
 for /R %dr% %%f in (*.java) do (
 	echo Ada file Java pada direktori %%f
 	echo %%~nxf ada, diganti namanya (y/t^)^?
 	set /p "vl=>"
+	@REM rename file java
 	if "!vl!" == "y" (
 		echo Input nama baru
 		set /p "rn=>"
